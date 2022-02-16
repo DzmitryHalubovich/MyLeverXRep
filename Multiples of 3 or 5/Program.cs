c512b950bf943ce7338bc4bd395afd4b0a1fed72
DzmitryHalubovich <buxarik493@gmail.com>
2022-02-16 23:16:54 +0300

    3 or 5

---
+++ Multiples of 3 or 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Multiples_of_3_or_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            int numb = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sum = " + Sum(numb));
+        }
+
+        static int Sum(int number)
+        {
+            if (number < 0)
+                return 0;
+
+            int sum = 0;
+            for (int i = 1; i < number; ++i)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            return sum;
+        }
+    }
+}
